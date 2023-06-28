da1c828a3b807c9c4db8568f84601d6943d03db4
BDuncanson-17 <108036000+BDuncanson-17@users.noreply.github.com>
2023-06-28 10:25:21 -0500

    Update Util.cpp

--- src/Util.cpp
+++ src/Util.cpp
@@ -6,6 +6,46 @@
 #include <sstream>
 #include <stdexcept>
 
+#include <ctime>
+#include <chrono>
+#include <iostream>
+#include <sstream>
+
+
+
+Time::Time() {
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+    std::tm *local_time = std::localtime(&now_time);
+
+    year = (1900 + local_time->tm_year) % 100; // Get the last two digits
+    month = 1 + local_time->tm_mon;
+    day = local_time->tm_mday;
+    hours = local_time->tm_hour;
+    minutes = local_time->tm_min;
+    seconds = local_time->tm_sec;
+}
+
+void Time::PrintTimeStamp() {
+    std::cout << "Time Stamp: " << std::endl;
+    std::cout << "Year: " << year << std::endl;
+    std::cout << "Month: " << month << std::endl;
+    std::cout << "Day: " << day << std::endl;
+    std::cout << "Hours: " << hours << std::endl;
+    std::cout << "Minutes: " << minutes << std::endl;
+    std::cout << "Seconds: " << seconds << std::endl;
+}
+
+std::string Time::TimeToCSVSting() {
+    std::stringstream ss;
+    ss << month << "," << day << "," << year << "," 
+        << hours << "," << minutes << "," << seconds;
+    return ss.str();
+}
+
+
+
+
 std::unordered_map<std::string, int> Constants::g_MonthMap = {
             {"Jan", 1},
             {"Feb", 2},
